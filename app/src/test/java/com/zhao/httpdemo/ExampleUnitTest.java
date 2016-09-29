cb1dbca585b36025922c0d761d6d3f17db92c399
xiantaozhaowei <416150565@qq.com>
2016-09-29 11:58:55 +0800

    unit test

--- app/src/test/java/com/zhao/httpdemo/ExampleUnitTest.java
+++ app/src/test/java/com/zhao/httpdemo/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.zhao.httpdemo;
 
+import com.zhao.httpdemo.net.HttpUtil;
+
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +17,12 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("password", "12345");
+        params.put("phone_number", "3413999999999");
+        params.put("type", "1");
+        String json = HttpUtil.hashMapToJson(params);
+        System.out.println(json);
         assertEquals(4, 2 + 2);
     }
 }
